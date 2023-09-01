4b99bb8e6fb62ad59fa3193283e0ce7225210a6f
Bryan Walsh <bryanwalsh220@gmail.com>
2023-09-01 16:00:07 -0700

    updated code

--- checkerboard/server.py
+++ checkerboard/server.py
@@ -3,9 +3,9 @@ app = Flask(__name__)
 
 @app.route('/<int:x>/<int:y>/<color1>/<color2>')
 def checkers(x, y, color1, color2):
-    row= int(x)
-    column = int(y)
-    return render_template('index.html', row=row, column=column, color1=color1, color2=color2)
+    # row= int(x)
+    # column = int(y)
+    return render_template('index.html', row=x, column=y, color1=color1, color2=color2)
 
 
 if __name__=="__main__": 
